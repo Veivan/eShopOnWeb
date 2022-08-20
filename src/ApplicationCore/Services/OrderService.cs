3f250797aa7d81a0ebd44205653af1dae0035345
K.Olifirenko <konstantin_olifirenko@epam.com>
2022-08-20 21:50:09 +0500

    Order service restored

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
@@ -49,5 +52,47 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+
+        //await UploadOrderAsync(order);
+        await SendOrderMessageAsync(order);
+    }
+    private async Task UploadOrderAsync(Order order)
+    {
+        var body = order.ToJson<Order>();
+        HttpClient Client = new HttpClient();
+        var jsonContent = new StringContent(body, Encoding.UTF8, "application/json");
+        //var url = "http://localhost:7071/api/OrderUploader";
+        //var url = "https://orderuploaderapp.azurewebsites.net/api/OrderUploader";
+        //var url = "http://localhost:7062/api/OrderSaver";
+        var url = "https://ordersaverfunc20220714012345.azurewebsites.net/api/OrderSaver";
+        var response = await Client.PostAsync(url, jsonContent);
+        response.EnsureSuccessStatusCode();
+        var stringResponse = await response.Content.ReadAsStringAsync();
+    }
+
+    private async Task SendOrderMessageAsync(Order order)
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://karafsbusns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mTVVHPe/1OobK7LVr38LowTK3y/pJFBTiBMUfNpN4vw=";
+        const string QueueName = "orders";
+
+        await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+        await using ServiceBusSender sender = client.CreateSender(QueueName);
+        try
+        {
+            var body = order.ToJson<Order>();
+            var message = new ServiceBusMessage(body);
+            await sender.SendMessageAsync(message);
+        }
+        catch (System.Exception exception)
+        {
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
     }
 }
